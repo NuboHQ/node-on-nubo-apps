07f07cbfd12d031fd384c798dab5fa1e157b13aa
Nubo Developer <developer@nubo.org>
2020-10-10 15:46:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Node on Nubo Apps</h1>');
+  res.send('<h1>Node - Test 2</h1>');
 });
 
 app.listen(PORT, () => console.log(`> Ready on http://localhost:${PORT}`));
